98ffa1c2d0f97a7d42eed9ad2708dd366b79b2be
andrewdark <samsonov.a@ukr.net>
2019-04-01 16:27:51 +0300

    HOT FIX daoIMPL for SprinBoot Context

--- src/main/java/ua/pp/darknsoft/dao/implementation/CandidateDaoImpl.java
+++ src/main/java/ua/pp/darknsoft/dao/implementation/CandidateDaoImpl.java
@@ -1,11 +1,13 @@
 package ua.pp.darknsoft.dao.implementation;
 
+import org.springframework.stereotype.Repository;
 import ua.pp.darknsoft.dao.interfaces.CandidateDao;
 import ua.pp.darknsoft.domain.entity.Candidate;
 
+@Repository
 public class CandidateDaoImpl extends GenericDaoImpl<Candidate, Long> implements CandidateDao {
 
-    protected CandidateDaoImpl(Class<Candidate> clazz) {
-        super(clazz);
+    public CandidateDaoImpl() {
+        super(Candidate.class);
     }
 }
--- src/main/java/ua/pp/darknsoft/dao/implementation/GenericDaoImpl.java
+++ src/main/java/ua/pp/darknsoft/dao/implementation/GenericDaoImpl.java
@@ -2,6 +2,8 @@ package ua.pp.darknsoft.dao.implementation;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import ua.pp.darknsoft.dao.interfaces.GenericDao;
 
 import javax.persistence.EntityManager;
@@ -18,9 +20,11 @@ import java.util.List;
  *
  * @since 1.0
  */
+@Transactional
 public abstract class GenericDaoImpl<T, ID> implements GenericDao<T, ID> {
 
     private final Class<T> clazz;
+    @Autowired
     private EntityManager entityManager;
 
     protected GenericDaoImpl(Class<T> clazz) {
@@ -30,8 +34,7 @@ public abstract class GenericDaoImpl<T, ID> implements GenericDao<T, ID> {
     public EntityManager getEntityManager() {
         return entityManager;
     }
-
-    @PersistenceContext
+    
     public void setEntityManager(EntityManager entityManager) {
         this.entityManager = entityManager;
     }
