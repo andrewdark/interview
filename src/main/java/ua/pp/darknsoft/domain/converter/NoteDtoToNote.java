7153c84df6f0b23cf96f999041516e5c081fd483
andrewdark <samsonov.a@ukr.net>
2019-04-08 13:47:09 +0300

    Created NoteDtoToNote converter

---
+++ src/main/java/ua/pp/darknsoft/domain/converter/NoteDtoToNote.java
@@ -0,0 +1,38 @@
+package ua.pp.darknsoft.domain.converter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.lang.Nullable;
+import ua.pp.darknsoft.domain.dto.NoteDto;
+import ua.pp.darknsoft.domain.entity.Note;
+
+public class NoteDtoToNote implements Converter<NoteDto, Note> {
+    private final Object $lock = new Object[0];
+
+    private InterviewerDtoToInterviewer interviewerDtoConverter;
+
+    @Autowired
+    public NoteDtoToNote(InterviewerDtoToInterviewer interviewerDtoConverter) {
+        this.interviewerDtoConverter = interviewerDtoConverter;
+    }
+
+    @Nullable
+    @Override
+    public Note convert(NoteDto noteDto) {
+        synchronized ($lock) {
+            if (noteDto == null) {
+                return null;
+            }
+            final Note note = new Note();
+            note.setId(noteDto.getId());
+            note.setCriteria(noteDto.getCriteria());
+            note.setNote(noteDto.getNote());
+            note.setScore(noteDto.getScore());
+            if (noteDto.getInterviewerDto() != null) {
+                note.setInterviewer(interviewerDtoConverter.convert(noteDto.getInterviewerDto()));
+            }
+
+            return note;
+        }
+    }
+}
